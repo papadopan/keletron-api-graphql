c0ab2385ade19cdd0db679966c7200873f901623
papadopan <antonios.papadopan@gmail.com>
2022-06-24 16:01:52 +0200

    feat(login): added login mutation

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,7 +1,17 @@
-import { Field, ID, ObjectType } from "type-graphql";
+import { Field, ID, InputType, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class User {
   @Field(() => ID)
   id: number;
+}
+
+
+@InputType()
+export class LoginCredentials {
+  @Field(()=>String, {nullable: false})
+  email: string
+
+  @Field(()=> String, {nullable: false})
+  password: string
 }
--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,6 +1,8 @@
 import { Context } from "../types/context";
-import { Ctx, Query, Resolver } from "type-graphql";
-import { User } from "../entities/User";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
+import { LoginCredentials, User } from "../entities/User";
+import { AuthenticationError, UserInputError } from "apollo-server-core";
+import argon2 from "argon2";
 
 @Resolver()
 export class UserResolver {
@@ -11,5 +13,23 @@ export class UserResolver {
   ): Promise<User | null> {
     const myself = await db.user.findUnique({where:{email:"antonios.papadopan@gmail.com"}})
     return myself;
+  }
+
+  @Mutation(()=>User)
+  async login(
+    @Arg("credentials") credentials: LoginCredentials,
+    @Ctx() {db}: Context
+  ): Promise<User>{
+    const {email, password} = credentials;
+
+    const user = await db.user.findUnique({where:{email: email}})
+    // if the user does not exist, it means that there is no such email
+    // in the db, therefore it is an authentication error
+    if(!user) throw new AuthenticationError("This email does not exist")
+    const isPasswordValid = await argon2.verify(user.password, password)
+    // in case the password
+    if(!isPasswordValid) throw new UserInputError("Password is incorrect")
+
+    return user
   }
 }
