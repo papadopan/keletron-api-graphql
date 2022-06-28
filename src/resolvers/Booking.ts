9fcab74e8a7a1730c6d6453f550d9357f074484a
papadopan <antonios.papadopan@gmail.com>
2022-06-28 09:15:49 +0200

    feat(prettier-eslint): add rules to the project

--- src/resolvers/Booking.ts
+++ src/resolvers/Booking.ts
@@ -1,9 +1,9 @@
-import { Query, Resolver } from "type-graphql";
+import { Query, Resolver } from 'type-graphql'
 
 @Resolver()
 export class BookingResolver {
-@Query(()=> String)
-async me(): Promise<string> {
-  return "this is it "
-}
+  @Query(() => String)
+  async me(): Promise<string> {
+    return 'this is it '
+  }
 }
