de5f457da2a22194e399cb5964661a9a4bee5855
Antonios Papadopoulos <antonios@smallpdf.com>
2023-09-12 15:43:00 +0200

    Update the registrationToken to a test one

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 import * as serviceAccount from './serviceAccountKey.json';
 
 const registrationToken =
-  'fx_XBjNkrEbHl2Y39O7T6g:APA91bFdxpzAAVucNj94EsiTxqMTIRuHo8Y8yjj2VmE7YJbZHSpqD0AF45FCQqPTWM_tzsgB-IUUxpJTOycSVDvt-_89jQKLcWrZnRe9F6n0V5uQDhr0-w-yqvnqlnNCKTiHy4P6a4ni';
+  'ePYHp6iNRkCijSM7WuQDit:APA91bFekAj5KYno1DQBPFN7d57TIChpBgv28X3JQwGnGM8Zu34UMropJmVFrzEGLuUIp_9Y_Uf83Mw1hktfuSQPUkK_2fHxtOlLVG44vh9J2Uos9Qqo1fIpEPpfQAGeE3-p-tUEpYVr';
 
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
