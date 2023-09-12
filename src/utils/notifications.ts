d0a4d348c27440a957aa895b52c2fa8b67192a4f
Antonios Papadopoulos <antonios@smallpdf.com>
2023-09-12 16:07:22 +0200

    test with sound

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -17,10 +17,15 @@ export const sendNotification = () => {
         title: 'Hello',
         body: 'Hello, world!',
       },
+      data: {
+        score: '850',
+        time: '2:45',
+      },
       apns: {
         payload: {
           aps: {
             sound: 'default',
+            badge: 42,
           },
         },
       },
